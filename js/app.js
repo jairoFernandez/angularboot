482b3aacb5eaae064f52e4538516341d08b20e2a
Jairo Fernandez <jairofdezvega@gmail.com>
2014-12-29 21:45:20 -0500

    Intento 1 DB

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 
-var zoo = angular.module('zooApp',['ngRoute']);
+var agenda = angular.module('agendaApp',['ngRoute']);
         
-        zoo.controller('controllerAnimales',['$scope', function($scope){
+        agenda.controller('controllerAnimales',['$scope', function($scope){
             $scope.animales = ['Andres fernandez','Mathias Gil','Pepito perez','Calamar']
             $scope.mostrarAnimales = function(){
                 $scope.animales.push($scope.animalextra)
+                
             }
         }]);
         
-		zoo.config([ '$routeProvider' , function($routeProvider){
+		agenda.config([ '$routeProvider' , function($routeProvider){
             $routeProvider.
             when('/inicio',{
                 templateUrl: 'plantillas/inicio.html',
@@ -25,110 +26,134 @@ var zoo = angular.module('zooApp',['ngRoute']);
             })
         }])
 
-zoo.controller('todoSqliteCtrl', ['$scope', 'todoService', function($scope, todoService)
-{
- 
-    todoService.getItems().then(function(items)
-    {
-        $scope.items = items;
-    });
- 
-    $scope.addItem = function(item)
-    {  
-        todoService.setItem(item);
-        todoService.getItems().then(function(items)
+      
+        agenda.controller('todoSqliteCtrl', ['$scope', 'todoService', function($scope, todoService)
         {
-            $scope.items = items;
-        });
-    }
- 
-    $scope.removeItem = function(id)
-    {
-        todoService.removeItem(id);
-        todoService.getItems().then(function(items)
-        {
-            $scope.items = items;
-        });
-    }
- 
-    $scope.removeAll = function()
-    {
-        todoService.removeAll();
-        todoService.getItems().then(function(items)
-        {
-            $scope.items = items;
-        });
-    }
- 
-    $scope.populate = function()
-    {
-        db.transaction(function(tx) 
-        {
-            tx.executeSql('CREATE TABLE IF NOT EXISTS todos(id integer primary key, todo text)');
-        });
-    }
-}]);
- 
-zoo.service('todoService', function($q) 
-{
-    this.getItems = function() 
-    {
-        var deferred, result = [];
-        deferred = $q.defer();
-        db.transaction(function(tx) 
-        {
-            tx.executeSql('CREATE TABLE IF NOT EXISTS todos(id integer primary key, todo text)');
-            tx.executeSql("select * from todos", [], function(tx, res) 
+
+            todoService.getItems().then(function(items)
             {
-                for(var i = 0; i < res.rows.length; i++)
+                $scope.items = items;
+            });
+
+            $scope.addItem = function(item)
+            {  
+                todoService.setItem(item);
+                todoService.getItems().then(function(items)
                 {
-                    result.push({id : res.rows.item(i).id, todo : res.rows.item(i).todo})
-                }
-                deferred.resolve(result);
-            });
-        });
-        return deferred.promise;
-    },
-    this.setItem = function(item) 
-    {
-        db.transaction(function(tx) 
-        {
-            tx.executeSql('CREATE TABLE IF NOT EXISTS todos(id integer primary key, todo text)');
-            return tx.executeSql("INSERT INTO todos (todo) VALUES (?)", [item], function(tx, res) 
+                    $scope.items = items;
+                });
+            }
+
+            $scope.removeItem = function(id)
             {
-                return true;
-            });
-        });
-        return false;
-    },
-    this.removeItem = function(id) {
-        db.transaction(function(tx) 
-        {
-            return tx.executeSql("DELETE FROM todos WHERE id = " + id, [], function(tx, res) 
+                todoService.removeItem(id);
+                todoService.getItems().then(function(items)
+                {
+                    $scope.items = items;
+                });
+            }
+
+            $scope.removeAll = function()
             {
-                return true;
-            });
-        });
-        return false;
-    },
-    this.removeAll = function() 
-    {
-        db.transaction(function(tx) 
+                todoService.removeAll();
+                todoService.getItems().then(function(items)
+                {
+                    $scope.items = items;
+                });
+            }
+
+            $scope.populate = function()
+            {
+                db.transaction(function(tx) 
+                {
+                    	var sql = "CREATE TABLE IF NOT EXISTS agenda_curso ( "+
+                                    "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                    "nombre VARCHAR(50), " +
+                                    "apellidos VARCHAR(50), " +
+                                    "telefono VARCHAR(30), " +
+                                    "categoria VARCHAR(30), " +
+                                    "foto VARCHAR(200), " + 
+                                    "email VARCHAR(30)," +
+                                    "peticion VARCHAR(255), "+
+                                    "ciudad VARCHAR(50), "+
+                                    "direccion VARCHAR(255), "+
+                                    "genero VARCHAR(20))";
+		
+                        tx.executeSql(sql);
+
+                        tx.executeSql("INSERT INTO agenda_curso (id,nombre,apellidos,telefono,categoria,foto,email,peticion,ciudad,direccion,genero) VALUES (1,'José','Pérez','+34566222666','amigo','','paco@paco.com','Hogar','Tunja','calle falsa 123','Hombre')");
+                        tx.executeSql("INSERT INTO agenda_curso (id,nombre,apellidos,telefono,categoria,foto,email,peticion,ciudad,direccion,genero) VALUES (2,'Siro','González','+34555434567','familia','','siro@test.com','Finanzas','Duitama','calle falsa 123','Hombre')");
+                        tx.executeSql("INSERT INTO agenda_curso (id,nombre,apellidos,telefono,categoria,foto,email,peticion,ciudad,direccion,genero) VALUES (3,'Julio','Rodríguez','+34756222666','trabajo','','julio@test.com','Santidad','Tunja','calle falsa 123','Hombre')");
+                                        tx.executeSql('CREATE TABLE IF NOT EXISTS AGENDA(id integer primary key, nombres text, apellidos text, edad int, peticion text)');
+                                        tx.executeSql()
+                });
+            }
+        }]);
+
+        app.service('todoService', function($q) 
         {
-            return tx.executeSql("DROP TABLE todos", [], function(tx, res) {
-                return true;
-            });
-        });
-        return false;
-    }
-});        
-        
-        
-        
-var db;
-    document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() 
-    {
-        db = window.openDatabase('agendaDB', '1.0', 'TodoDB', 200000);
-        angular.bootstrap(document, ['app']);
-    }
+            this.getItems = function() 
+            {
+                var deferred, result = [];
+                deferred = $q.defer();
+                db.transaction(function(tx) 
+                {
+                    
+                    tx.executeSql("select * from AGENDA", [], function(tx, res) 
+                    {
+                        for(var i = 0; i < res.rows.length; i++)
+                        {
+                            result.push({id : res.rows.item(i).id, todo : res.rows.item(i).todo})
+                        }
+                        deferred.resolve(result);
+                    });
+                });
+                return deferred.promise;
+            },
+            this.setItem = function(item) 
+            {
+                db.transaction(function(tx) 
+                {
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS AGENDA(id integer primary key, nombres text, apellidos text, edad int, peticion text)');
+                    return tx.executeSql("INSERT INTO AGENDA(nombres, apellidos, edad, peticion) VALUES (?,?,?,?)", [item], function(tx, res) 
+                    {
+                        return true;
+                    });
+                });
+                return false;
+            },
+            this.removeItem = function(id) {
+                db.transaction(function(tx) 
+                {
+                    return tx.executeSql("DELETE FROM todos WHERE id = " + id, [], function(tx, res) 
+                    {
+                        return true;
+                    });
+                });
+                return false;
+            },
+            this.removeAll = function() 
+            {
+                db.transaction(function(tx) 
+                {
+                    return tx.executeSql("DROP TABLE todos", [], function(tx, res) {
+                        return true;
+                    });
+                });
+                return false;
+            }
+        });       
+
+        // Creación de DB
+        var db;
+            document.addEventListener("deviceready", onDeviceReady, false);
+            function onDeviceReady() 
+            {
+                db = window.openDatabase('agendaDB', '1.0', 'agendaDB', 200000);
+                angular.bootstrap(document, ['app']);
+            }
+
+
+                                  
+                                  
+
